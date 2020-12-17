dff56679e9c3fafd37a3fcf26053b3eb8d089b7f
Kathie Yan Sun <kys6@longleaf-login1.its.unc.edu>
2020-12-16 19:50:48 -0500

    longleaf

--- stan_pheno.R
+++ stan_pheno.R
@@ -1,20 +1,23 @@
 library(tidyverse)
 library(rstan)
 
-setwd("C:/Users/Kathie/TReC_matnut/src")
+#setwd("C:/Users/Kathie/TReC_matnut/src")
+setwd("~/TReC_matnut/src")
 
 source("lmer_functions_rna.R")
 source("stan_pheno_functions.R")
-source("prediction_functions.R")
+#source("prediction_functions.R")
 source("summary_functions.R")
 
+args <- commandArgs(trailingOnly = TRUE) 
+it = as.numeric(args[1])
 
 ###### Read in data
 #dir <- "C:/Users/Kathie/Dropbox\ (ValdarLab)"
 dir <- "/nas/depts/006/valdar-lab/users/sunk/"
 matnut = read.csv(file.path(dir,'matnut_main/AllMice_GeneExpression_SSupdated_11.27.19.csv'))
 #matnut <- readRDS(file.path(dir,'phenotype_analysis/matnut_data.rds'))
-genes = read.csv("deseq2/priorityTryGenes_16dec2020.csv")
+genes = read.csv("../deseq2/priorityTryGenes_16dec2020.csv", header=F)
 
 
 
@@ -29,12 +32,17 @@ gene_count$Gene.Name = do.call("rbind", (strsplit(as.character(gene_count$gene_i
 samples = colnames(gene_count)[grep("Pup", colnames(gene_count))]
 matnut$ID = paste0("Pup.ID_",matnut$Pup.ID)
 matnut$Diet = gsub(" $", "", matnut$Diet)
+matnut$Diet = factor(matnut$Diet, levels=c("Standard","Low Protein","Methyl Enriched","Vitamin D Deficient"))
 matnut$RIX = gsub("a|b","",matnut$Reciprocal)
+matnut$RIX = factor(matnut$RIX, levels=c(1:4,6:10))
 matnut$PO = ifelse(gsub("[0-9]","",matnut$Reciprocal) == "a", 0.5, -0.5)
 matnut$DietRIX = gsub(" ", ".",paste0(matnut$Diet, matnut$RIX))
+levs = apply(expand.grid(gsub(" ",".",levels(matnut$Diet)), levels(matnut$RIX)),1,function(x) paste(x, collapse=""))
+matnut$DietRIX = factor(matnut$DietRIX, levels = levs[which(levs%in%matnut$DietRIX)])
 matnut$DietRIXPOq = paste0(matnut$DietRIX,"_",matnut$PO)
 matnut$RIX = factor(matnut$RIX, levels = c(1:4,6:10))
 map = read.csv(file.path(dir,"mini/combined_map_cs_gbrs_allmarkers_17mar2020.csv"))
+encoded <- getEncoding(matnut, terms = c("RIX","Diet","DietRIX"))
 
 annot = read.table(file.path(dir, "mm10/ref_sequence/Mus_musculus.GRCm38.96.gtf"), skip=5, fill=T)
 #annot = read.table(file.path(dir, "matnut_main/Mus_musculus.GRCm38.96.gtf"), skip=5, fill=T)
@@ -52,119 +60,30 @@ if(length(which(duplicated(annot_genes$Gene.Name))) > 0){
 
 annot_genes = annot_genes[which(annot_genes$Gene.ID %in% gene_count$Gene.ID |annot_genes$Gene.Name %in% gene_count$Gene.Name),]
 
+annot_genes = annot_genes %>% filter(Gene.Name %in% genes$V1)
 
-regions_list = readRDS(file.path(dir,"mini/seg_regions_by_genotyping_perRIX_kmerBased_23nov2019.rds"))
-haplofiles = list.files(file.path(dir,"mini/pup_haplo_blocks_by_CC_parent_dec2019"), 
-                        pattern="haploBlocks.rds", full.names = T)
-phased_par_CC_haplotypes = lapply(haplofiles, readRDS)
-tmp = do.call("rbind", strsplit(haplofiles, "_"))
-names(phased_par_CC_haplotypes) = tmp[,ncol(tmp)-1]
+t_counts = data.frame(t(gene_count %>% filter(Gene.Name %in% annot_genes$Gene.Name) %>%
+			select(contains("Pup.ID")))) 
+tmp = gene_count$Gene.Name[which(gene_count$Gene.Name %in% annot_genes$Gene.Name)] 
+tmp[46] = "Snhg14_v2"
+colnames(t_counts) = tmp 
+t_counts$ID = unlist(as.character(rownames(t_counts)))
+matnut_use = right_join(matnut, t_counts, "ID")
 
-maxn = floor(nrow(annot_genes)/500)
-its_1 = ((it-1)*500)+1
-its_2 = ifelse(it==maxn, nrow(annot_genes), it*500)
+
+#regions_list = readRDS(file.path(dir,"mini/seg_regions_by_genotyping_perRIX_kmerBased_23nov2019.rds"))
+#haplofiles = list.files(file.path(dir,"mini/pup_haplo_blocks_by_CC_parent_dec2019"), 
+#                        pattern="haploBlocks.rds", full.names = T)
+#phased_par_CC_haplotypes = lapply(haplofiles, readRDS)
+#tmp = do.call("rbind", strsplit(haplofiles, "_"))
+#names(phased_par_CC_haplotypes) = tmp[,ncol(tmp)-1]
+
+maxn = floor(nrow(annot_genes)/10)
+its_1 = ((it-1)*10)+1
+its_2 = ifelse(it==maxn, nrow(annot_genes), it*10)
 
 print(paste(its_1,its_2))
-stanlist <- lapply(matnut$ptypes[7], function(x) stanSum(df=matnut$df, encoded=matnut$encoded, phenotype=x, 
-                   randvar=c("DamID", "RIX", "DietRIX"), fixvar="Diet", POvar=c("RIX", "Diet:RIX"),
-                   tryLam=c(-1, 0, .25, .33, .5, 1, 2, 3), normd=T, 
-                   chains=1, iter=2000))
 
-
-  stanmcmc<- As.mcmc.list(resStan)
-  summcmc <- summary(stanmcmc)
-  
-  #armform <- y ~ Diet + PO*RIX + Diet*RIX + PO*Diet*RIX
-  #test_stanarm = stan_glm(armform, data=df, prior = lasso(autoscale = T))
-  
-  par(mfrow=c(3,3))
-  coda::traceplot(stanmcmc[[1]][,grep("SPO[[]", colnames(stanmcmc[[1]])),drop=F])
-  coda::traceplot(stanmcmc[[1]][,grep("sigma|lambda", colnames(stanmcmc[[1]])),drop=F])
-  tset <- sapply(1:(ncol(stanmcmc[[1]])-1), function(x) HPDinterval(stanmcmc[,x,drop=T]), simplify=F)
-
-  
-  
-  
-  
-  
-  
-reg.jags <- jags.model(textConnection(madeModel$modelFin), data=madeModel$data, n.chains = chains, n.adapt = n.adapt)
-update(reg.jags, n.iter=n.adapt)
-fitJags[[phenotype[i]]]$fit <- coda.samples(reg.jags, variable.names = madeModel$paramUse, thin=thin, n.iter=n.iter)
-fitJags[[phenotype[i]]]$madeModel <- madeModel
-allnames <- c()
-paramUse <- gsub("beta","", unique(unlist(
-  lapply(strsplit(varnames(fitJags[[phenotype[i]]]$fit),'[[]'), function(x) x[[1]]))))
-wantParam <- intersect(paramUse, c(paste(unique(encoded$Variable)), "grand_mu"))
-  
-  
-  
-  
-  
-  
-  jagsFit <- runJagsModels_cov(df=df, chains=chains, testLMER = lmerFit, 
-                               n.adapt=n.adapt, n.iter=n.iter, thin=thin, 
-                               encoded=encoded, phenotype=phenotype, sq=sq, addS=addS)
-  
-
-  
-  keep <- c("Diet", "DietRIX","PODietRIX","PORIX", "RIX")
-  lmerSum <- lmer.getDecodedSummary(lmerFit, lmerFit$LMformula, phenotypes=phenotype)
-  test_cm <- contrast_matrix(encoded)
-  
-  jagsSum <- list()
-  allSummary <- list()
-  null.test <- list()
-  jagsLmer_compare <- list()
-  plotCat <- list()
-  madeMod <- list()
-  all.mcmc <- list()
-  
-  contrastMat <- contrast_matrix(encoded, levels=c("RIX","Diet"))
-  
-  diet.test <- list()
-  rix.test<- list()
-  
-  for(i in 1:length(lmerFit$lmerobj$y.transform)){
-    all.mcmc[[phenotype[i]]] <- mcmc.stack(jagsFit[[i]]$fit)
-    jagsSum[[i]] <- jags.getDecodedSummary(all.mcmc[[phenotype[i]]], encoded)
-    jagsSum[[i]]$Level <- factor(jagsSum[[i]]$Level, levels=jagsSum[[i]]$Level)
-    if(!any(is.na(lmerSum[[i]]))) {
-      JLSum <- compareSummary(lmerSum[[i]], jagsSum[[i]], keep)
-      jagsLmer_compare[[phenotype[i]]] <- data.frame(phenotype=rep(phenotype[i],nrow(JLSum)), JLSum) 
-      keepLev <- unique(JLSum$Level)
-    } else {
-      jagsLmer_compare[[phenotype[i]]] <- NA
-      plot=F
-      keepLev <- jagsSum[[i]]$Level[which(jagsSum[[i]]$Variable %in% keep)]
-    }
-    
-    null.test[[phenotype[i]]]  <- contrast.null(dfObject=all.mcmc[[phenotype[i]]], levels=keepLev)
-    
-    null_p <- data.frame(Level = rownames(null.test[[phenotype[i]]]),
-                         pval = null.test[[phenotype[i]]]$pval, 
-                         signif = null.test[[phenotype[i]]]$signif)
-    null_p$Level <- factor(null_p$Level, levels=jagsSum[[i]]$Level)
-    null_p <- null_p[order(null_p$Level),]
-    mergeTable <- merge(jagsSum[[i]], null_p, by="Level")
-    mergeTable$Level <- factor(mergeTable$Level, levels=encoded$Level[which(encoded$Variable %in% keep)])
-    mergeTable$Variable <- factor(mergeTable$Variable, levels=unique(encoded$Variable)[which(encoded$Variable %in% keep)])
-    mergeTable <- mergeTable[order(mergeTable$Level),]
-    allSummary[[phenotype[i]]]  <- cbind(phenotype=rep(phenotype[i],nrow(mergeTable)), mergeTable[order(mergeTable$Variable, mergeTable$Level),])
-    
-    if(plot) plotCat[[phenotype[i]]] <- plotSummary(lmerSum[[i]], jagsSum[[i]], JLSum, phenotype)
-    madeMod[[phenotype[i]]]  <- jagsFit[[i]]$madeModel
-    
-    if(contrasts){
-      diet.test[[phenotype[i]]] <- contrast.test(data=all.mcmc[[phenotype[i]]], encoded, variable="Diet", byVar="RIX", contrast=contrastMat$Diet.contr)
-      rix.test[[phenotype[i]]] <- contrast.test(data=all.mcmc[[phenotype[i]]], encoded, variable="RIX", byVar="Diet", contrast=contrastMat$RIX.contr)
-    }
-  }
-  
-  return(list(transformation=transf, mcmcObject=all.mcmc, plot=plotCat, allSummary=allSummary,
-              null.test=null.test, diet.test=diet.test, rix.test=rix.test, S=S, modelDef=madeMod, 
-              JL_compare=jagsLmer_compare, lmer_obj = lmerFit))
-}
 
 
 getFormulas = function(fixvar, randvar, POvar){
@@ -199,3 +118,43 @@ getFormulas = function(fixvar, randvar, POvar){
   return(list(lmerform=lmerform, jagsform=jagsform))
 }
 
+
+
+
+
+stanlist <- lapply(as.character(genes$V1)[its_1:its_2], function(x) stanSum(df=matnut_use, encoded=encoded, phenotype=x, 
+                   randvar=c("RIX", "DietRIX"), fixvar="Diet", POvar=c("RIX", "DietRIX"),
+                   tryLam=c(-1, 0, .25, .33, .5, 1, 2, 3), normd=T, 
+                   chains=3, iter=6000))
+names(stanlist) = as.character(genes$V1)[its_1:its_2]
+saveRDS(stanlist, paste0(dir,"/trec/tot_expr_priorityGenes_",it,"_16dec2020.rds"))
+#  stanmcmc<- As.mcmc.list(resStan)
+#  summcmc <- summary(stanmcmc)
+  
+  #armform <- y ~ Diet + PO*RIX + Diet*RIX + PO*Diet*RIX
+  #test_stanarm = stan_glm(armform, data=df, prior = lasso(autoscale = T))
+  
+#  par(mfrow=c(3,3))
+#  coda::traceplot(stanmcmc[[1]][,grep("SPO[[]", colnames(stanmcmc[[1]])),drop=F])
+#  coda::traceplot(stanmcmc[[1]][,grep("sigma|lambda", colnames(stanmcmc[[1]])),drop=F])
+#  tset <- sapply(1:(ncol(stanmcmc[[1]])-1), function(x) HPDinterval(stanmcmc[,x,drop=T]), simplify=F)
+
+  
+  
+  
+  
+  
+  
+#reg.jags <- jags.model(textConnection(madeModel$modelFin), data=madeModel$data, n.chains = chains, n.adapt = n.adapt)
+#update(reg.jags, n.iter=n.adapt)
+#fitJags[[phenotype[i]]]$fit <- coda.samples(reg.jags, variable.names = madeModel$paramUse, thin=thin, n.iter=n.iter)
+#fitJags[[phenotype[i]]]$madeModel <- madeModel
+#allnames <- c()
+#paramUse <- gsub("beta","", unique(unlist(
+#  lapply(strsplit(varnames(fitJags[[phenotype[i]]]$fit),'[[]'), function(x) x[[1]]))))
+#wantParam <- intersect(paramUse, c(paste(unique(encoded$Variable)), "grand_mu"))
+  
+  
+  
+  
+  
