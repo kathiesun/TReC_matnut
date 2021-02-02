8d02ae89fdce48c5c40cc265d770101c99345c6b
Kathie Yan Sun <kys6@longleaf-login2.its.unc.edu>
2021-02-01 19:29:28 -0500

    longleaf

--- deseq2/deseq2_ll_combineFishers_HetRegions.R
+++ deseq2/deseq2_ll_combineFishers_HetRegions.R
@@ -1,6 +1,7 @@
 setwd("~/TReC_matnut/src")
 library(tidyverse)
 library(fdrtool)
+library(DESeq2)
 
 source("prediction_functions.R")
 source("summary_functions.R")
@@ -9,23 +10,47 @@ source("stan_pheno_functions.R")
 
 args <- commandArgs(trailingOnly = TRUE)
 c = as.numeric(args[1])
+n=1000
 
 dir <- "/nas/depts/006/valdar-lab/users/sunk/"
+annot = read.table(file.path(dir, "mm10/ref_sequence/Mus_musculus.GRCm38.96.gtf"), skip=5, fill=T)
+annot_genes = annot %>% filter(V15 == "gene_name") %>%
+	dplyr::select(one_of(paste0("V",c(1,4,5,7,10,16)))) %>%
+	distinct()
+
+colnames(annot_genes) = c("Chr","Start","End","Strand","Gene.ID","Gene.Name")
+annot_genes = annot_genes %>% mutate(Start = as.numeric(paste(Start)), End = as.numeric(paste(End)))
+    # 
+if(length(which(duplicated(annot_genes$Gene.Name))) > 0){
+	annot_genes = annot_genes[-which(duplicated(annot_genes$Gene.Name)),]
+}
+
+
+indiv_pups = list.files(file.path(dir, "mini/pup_haplo_blocks_by_CC_parent_jan2021"), pattern="haploBlocks", full.names = T)
+
+phased_CC_haplotype = lapply(indiv_pups, readRDS)
+tmp = do.call("rbind", lapply(indiv_pups, function(x) unlist(strsplit(x, "_"))))
+names(phased_CC_haplotype) = paste0("Pup.ID_", tmp[,ncol(tmp)-1])
 
 dds_list = readRDS(file.path(dir,"de_results/dds_list_allReg_sepRIX_30jan2021.rds"))
 
 POres_list = lapply(dds_list, function(x) results(x, name="PO"))
 POres_Ordered <- lapply(POres_list, function(x) x[order(x$pvalue),])
-sig_genes_list = lapply(POres_Ordered, function(x) x[which(x$padj < 0.1),])
+all_genes = unique(unlist(lapply(POres_list, function(x) rownames(x))))
+use_genes = all_genes[((c-1)*n + 1):min(length(all_genes), (c-1)*n + n)]
+
+sig_genes_list = lapply(POres_list, function(x) x[which(rownames(x) %in% use_genes),])
 #sig_genes_short = lapply(sig_genes_list, function(x) 
 #  if(length(grep("Gm|Rik|[.]", x)) > 0) x[-grep("Gm|Rik|[.]", x)])
-deseq_sig = sort(table(unlist(lapply(POres_Ordered, function(x) rownames(x[which(x$padj < 0.1),])))), decreasing = T)
+#deseq_sig = sort(table(unlist(lapply(POres_Ordered, function(x) rownames(x[which(x$padj < 0.1),])))), decreasing = T)
 
 for(i in 1:length(sig_genes_list)){
   sig_genes_list[[i]] = data.frame(sig_genes_list[[i]])
-  sig_genes_list[[i]]$gene = rownames(sig_genes_list[[i]])
-  sig_genes_list[[i]]$rix = names(sig_genes_list)[i]
-  rownames(sig_genes_list[[i]]) = NULL
+  if(nrow(sig_genes_list[[i]]) > 0){
+	sig_genes_list[[i]]$gene = rownames(sig_genes_list[[i]])
+  	sig_genes_list[[i]]$rix = names(sig_genes_list)[i]
+  	rownames(sig_genes_list[[i]]) = NULL
+  }
 }
 
 sig_genes_df = do.call("rbind", sig_genes_list)
@@ -39,9 +64,9 @@ for(g in unique(sig_genes_df$gene)){
   p_tmp = do.call("rbind", p_tmptmp)
   p_tmp$gene = g
   p_tmp$RIX = names(p_tmptmp)[which(unlist(lapply(p_tmptmp, nrow)) > 0)]
-  chec_het = data.frame(counts = t(gene_count[which(rownames(gene_count) == g),]), 
-                        Pup = colnames(gene_count_red))
-  chec_het$Pup.ID = as.numeric(unlist(strsplit(chec_het$Pup, "_"))[c(F,T)])
+  #chec_het = data.frame(counts = t(gene_count[which(rownames(gene_count) == g),]), 
+  #                      Pup = colnames(gene_count_red))
+  #chec_het$Pup.ID = as.numeric(unlist(strsplit(chec_het$Pup, "_"))[c(F,T)])
   gene_det = annot_genes %>% filter(Gene.Name == g)
   if(gene_det$Chr %in% c(1:19)){
     pup_haps = do.call("rbind", lapply(phased_CC_haplotype, function(x) {  
@@ -99,16 +124,7 @@ for(g in unique(sig_genes_df$gene)){
 }
 
 
+saveRDS(which_homs_list, paste0(dir,"/de_results/het_regions/which_homs_list_",c,".rds"))
+saveRDS(pmat_list, paste0(dir,"/de_results/het_regions/pmat_list_",c,".rds"))
 
 
-pmat_df = do.call("rbind", pmat_list)
-pmat_df$gene = rownames(pmat_df)
-fisher_ps = apply(pmat_df, 1, function(x)
-  c(p_het = pchisq(as.numeric(x["het_stat"]), df=(as.numeric(x["het_n"])*2),lower.tail = F),
-    p_hom = pchisq(as.numeric(x["hom_stat"]), df=(as.numeric(x["hom_n"])*2),lower.tail = F)))
-pmat_df = cbind(pmat_df, t(fisher_ps))
-
-fdr = fdrtool(x = pmat$fisher_p, statistic = "pvalue")
-pmat$padj_qval = as.numeric(fdr$qval)
-pmat$padj_fdr  = as.numeric(fdr$lfdr)
-pmat$padj = as.numeric(p.adjust(pmat$fisher_p, method = "BH"))
