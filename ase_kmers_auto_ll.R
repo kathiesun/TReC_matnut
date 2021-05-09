e8a2f463960a3c59e54d39690a065b0563a957a8
Kathie Yan Sun <kys6@longleaf-login2.its.unc.edu>
2021-05-09 15:29:52 -0400

    longleaf

--- ase_kmers_auto_ll.R
+++ ase_kmers_auto_ll.R
@@ -24,12 +24,13 @@ data_kmers_list = readRDS(file.path(dir, "trec/data_kmers_from_process_and_plot/
 #for (r in c(2:4,6:10)){
 	test_dat = do.call("rbind", lapply(data_kmers_list, function(x) x %>% filter(RRIX == r)))
 	test_dat = test_dat %>% group_by(Pup.ID, seq.Gene, Diet, Reciprocal, pup_gene) %>% 
-	      		summarize(mat_tot = sum(CC_1), pat_tot = sum(CC_2)) %>%
-	          	mutate(Pup.ID = factor(Pup.ID), 
-				seq.Gene = factor(seq.Gene), 
-				Diet = factor(Diet), 
-				Reciprocal = factor(Reciprocal))
-    	test_dat$pup_gene = factor(test_dat$pup_gene)
+		summarize(mat_tot = sum(CC_1), pat_tot = sum(CC_2)) %>%
+		mutate(Pup.ID = factor(Pup.ID), 
+		       seq.Gene = factor(seq.Gene), 
+			Diet = factor(Diet, levels= c("Standard","LowProtein","MethylEnriched","VitaminDDeficient")), 
+			Reciprocal = factor(Reciprocal))
+	test_dat$PO = unlist(lapply(test_dat$Reciprocal, function(x) ifelse(length(grep("a", x)) > 0, 0.5, -0.5)))
+   	test_dat$pup_gene = factor(test_dat$pup_gene)
       
       	test_dat$tot = test_dat$mat_tot + test_dat$pat_tot
       	encoded = getEncoding(test_dat, terms=c("Pup.ID","seq.Gene", "Diet", "Reciprocal", "pup_gene") )
@@ -37,12 +38,12 @@ data_kmers_list = readRDS(file.path(dir, "trec/data_kmers_from_process_and_plot/
         fit_list = list()
         for (g in encoded$Level[which(encoded$Variable == "seq.Gene")]){
 		temp_dat = test_dat %>% filter(seq.Gene == g)
-		try(fit_list[[g]] <- vglm(cbind(mat_tot, pat_tot) ~ 1, betabinomial (zero = 2),
+		try(fit_list[[g]] <- vglm(cbind(mat_tot, pat_tot) ~ PO + Diet, betabinomial,
 			data = temp_dat, trace = TRUE),
 		        silent=T)
 	        
 	        #coef(fit2)
 	}
 	
-	saveRDS(fit_list, paste0(dir, "/ase_vgam/ase_intOnly_rix",r,"_1may2021.rds"))  
+	saveRDS(fit_list, paste0(dir, "/ase_vgam/ase_PODIET_rix",r,"_1may2021.rds"))  
 #}
